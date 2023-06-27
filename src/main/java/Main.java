c405bc10d31749896f47a5e88080b1dbad8c2e31
اَمیرمَمّد <amirmmd.ltf@stu.nit.ac.ir>
2023-06-27 19:20:31 +0330

    proccesing setup completed

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,24 @@
-public class Main {
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+    public static PApplet pro;
+
+    // Constants
+    final int WINDOW_WIDTH = 900;
+    final int WINDOW_HEIGHT = 700;
+
+    public void settings() {
+        size(WINDOW_WIDTH, WINDOW_HEIGHT);
+    }
+
+    public void setup() {
+        pro = this;
+    }
+
+    public void draw() {
+    }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        PApplet.main("Main");
+
     }
 }
